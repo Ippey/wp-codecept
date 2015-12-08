77b7649eb36da82b2b83025c9b4d1df543e405c8
Ippei Sumida <ippey.s@me.com>
2015-12-09 00:18:25 +0900

    first commit

---
+++ wp-codecept.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wp-codecept
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: wp-codecept
+Domain Path: /languages
+*/
+
